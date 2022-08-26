ad0048775a88978d57b15da8b8337f0916771789
Sathvik <88944590+User319183@users.noreply.github.com>
2022-08-26 10:45:45 -0400

    Update anti-spam.py
    WIP (Work in progress)

--- cogs/anti-spam.py
+++ cogs/anti-spam.py
@@ -26,91 +26,49 @@ from discord import Message
 
 import aiohttp
 
+from pymongo import MongoClient
+
+from discord import option
+
 
 class AntiSpam(commands.Cog):
-    """A simple, basic cog."""
 
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot):
         self.bot = bot
 
 
+    
+    
 
 
+# let's work on anti-spam later. We need to work on how we will do the on_message event.
 
 
-
-
-
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def spam_mode_on(self, ctx: commands.Context):
-
-        with open("cogs/spammode.txt", "w+") as file:
-                file.truncate(0)
-                file.write("on")
-                await ctx.send("Spam-Mode has been activated. Ready to protect.")
-                
-                
-                
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def spam_mode_off(self, ctx: commands.Context):
-
-        with open("cogs/spammode.txt", "w+") as file:
-                file.truncate(0)
-                file.write("off")
-                await ctx.send("Spam-Mode has been turned off. Protection has been lost.")
-
-
-
-
-    @commands.Cog.listener()
-    async def on_message(self, message: Message):
-        if message.author.bot:
-            return
-
-        with open("cogs/spammode.txt", "r+") as file:
+    # @commands.slash_command()
+    # @commands.has_permissions(administrator=True)
+    # async def antispam(self, ctx, option: Option(str, "Should we enable or disable antispam mode?", choices=["Enable", "Disable"])):
         
-            for lines in file:
-                if "on" in lines:
-
-                    counter = 0
-                    with open("cogs/spamdetect.txt", "r+") as file:
-                        for lines in file:
-                            if lines.strip("\n") == str(message.author.id):
-                                counter+=1
-
-                        file.writelines(f"{str(message.author.id)}\n")
-
-                        if counter > 5:
-                            await message.author.send("Spam is prohibited. You have been kicked.")
-                            await message.author.kick()
-                            print("action = kicked")
-                            await message.channel.purge(limit=5)
-                
-                
-                            
-                            try:
-
-                                channel = discord.utils.get(message.guild.text_channels, name="spam-logs")
-                                message.author
-
-                                embed = discord.Embed(title="Member Kicked", description=f"{message.author.mention} has been punished for spam.", color=0xD708CC)
-                                embed.add_field(name="Action", value=f"Kicked", inline=False)
-                                embed.timestamp = discord.utils.utcnow()
-                                await channel.send(embed=embed)
-                            except:
-                                pass
-                            
-                            
-                else:
-                    break
+        # client = MongoClient("mongodb+srv://User:PASSWORD@cluster0.fdd0q.mongodb.net/DB_NAME?retryWrites=true&w=majority")
+        # db = client.THEDATABASE
+        # collection = db.antispam
+        
 
 
+    #     if option == "Enable":
 
+    #         collection.replace_one({"guild_id": ctx.guild.id}, {"guild_id": ctx.guild.id, "option": "Enable"}, upsert=True)
 
+    #         await ctx.respond(f"Anti-Spam has been enabled for `{ctx.guild.name}`.")
 
+    #     elif option == "Disable":
+    #         collection.replace_one({"guild_id": ctx.guild.id}, {"guild_id": ctx.guild.id, "option": "Disable"}, upsert=True)
+    #         await ctx.respond(f"Anti-Spam has been disabled for `{ctx.guild.name}`.")
+            
+            
+            
+            
 
-def setup(bot: commands.Bot):
-    bot.add_cog(AntiSpam(bot))
+            
+            
+def setup(bot):
+	bot.add_cog(AntiSpam(bot))
